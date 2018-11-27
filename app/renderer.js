18b79a27ddcceb62de9a5a8d97514e05184336b3
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-27 17:31:38 +0400

    added Items components and now the render is correct and togglable

--- app/renderer.js
+++ app/renderer.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const Application = () => {
-	return (
-		<div>
-			<h1>Hello World!</h1>
-			<button className="full-width">
-				This Button does not do anything
-			</button>
-		</div>
-	);
-};
+import Application from './components/Application';
 
 render(<Application />, document.getElementById('application'));
