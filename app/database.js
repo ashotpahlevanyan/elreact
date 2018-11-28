5eb46084e1d6c31a2dfccdaf03003e57454adf6e
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-28 08:42:50 +0400

    added items table creation if there is no one

--- app/database.js
+++ app/database.js
@@ -9,4 +9,15 @@ const database = knex({
 	useNullAsDefault: true
 });
 
+
+database.schema.hasTable('items').then(exists => {
+	if(!exists) {
+		return database.schema.createTable('items', t => {
+			t.increments('id').primary();
+			t.string('value', 100);
+			t.boolean('packed');
+		});
+	}
+});
+
 export default database;
