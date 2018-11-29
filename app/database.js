7f702b9e1335035a2d5266502bf7fc8a5accbdb5
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-11-29 16:11:02 +0400

    cleanup of database file

--- app/database.js
+++ app/database.js
@@ -64,34 +64,3 @@ export default {
 			});
 	}
 };
-
-//
-// import 'sqlite3';
-// import knex from 'knex';
-// import * as path from 'path';
-// import { remote } from 'electron';
-// const app = remote.app;
-//
-// const database = knex({
-// 	client: 'sqlite3',
-// 	connection: {
-// 		filename: path.join(
-// 			app.getPath('userData'),
-// 			'jetsetter-items.sqlite'
-// 		)
-// 	},
-// 	useNullAsDefault: true
-// });
-//
-//
-// database.schema.hasTable('items').then(exists => {
-// 	if(!exists) {
-// 		return database.schema.createTable('items', t => {
-// 			t.increments('id').primary();
-// 			t.string('value', 100);
-// 			t.boolean('packed');
-// 		});
-// 	}
-// });
-
-//export default database;
